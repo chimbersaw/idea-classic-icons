9dda035ab83cf07021fa9084b545c0d67a89b186
Maksim Sukhodolskii <maksukh@inbox.ru>
2024-09-26 01:20:37 +0200

    Get rid of internal api usages & add icon

--- src/main/kotlin/ru/chimchima/IconChanger.kt
+++ src/main/kotlin/ru/chimchima/IconChanger.kt
@@ -1,21 +1,18 @@
 package ru.chimchima
 
-import com.intellij.ide.ApplicationLoadListener
-import com.intellij.openapi.application.Application
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.application.PathManager
-import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.ui.MacCustomAppIcon
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 
-@Suppress("UnstableApiUsage")
-class IconChanger : ApplicationLoadListener {
-    private val logger = Logger.getInstance(IconChanger::class.java)
+class IconChanger : AppLifecycleListener {
+    private val logger = logger<IconChanger>()
 
-    override suspend fun beforeApplicationLoaded(application: Application, configPath: Path) {
+    override fun appFrameCreated(commandLineArgs: List<String?>) {
         if (SystemInfo.isMac) {
             println("Setting custom icon...")
             val appPath = PathManager.getHomePath()
